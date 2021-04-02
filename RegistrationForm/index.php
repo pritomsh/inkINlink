caea26f3abbfd3044b6c0b3baf785b6c200c97fb
pritom1807 <pritom15-1807@diu.edu.bd>
2021-04-02 13:41:05 +0600

    and registration form

---
+++ RegistrationForm/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/design.css" type="text/css">
+
+</head>
+
+<body>
+
+    <div class="registration">
+        <div class="form-conteniar">
+            <form class="box">
+                <h2>Registration</h2>
+                
+                
+                <div class="input-name">
+                    <i class="fa fa-user lock"></i>
+                    <input type="text" placeholder="First Name" class="name">
+                </div>
+
+
+                <div class="input-name">
+                    <i class="fa fa-user lock"></i>
+                    <input type="text" placeholder="Last Name" class="name">
+                </div>
+
+
+
+                <div class="input-name">
+                    <i class="fa fa-envelope email"></i>
+                    <input type="email" placeholder="Email" required class="text-name">
+                </div>
+
+                <div class="input-name">
+                    <i class="fa fa-check username"></i>
+                    <input type="text" placeholder="username" required class="text-name" class="username">
+                </div>
+
+                <div class="input-name">
+                    <i class="fa fa-lock lock"></i>
+                    <input type="password" placeholder="Password" class="text-name">
+                </div>
+
+
+
+                <div class="input-name">
+                    <i class="fa fa-lock lock"></i>
+                    <input type="password" placeholder="Re-type Password" class="text-name">
+                </div>
+
+
+
+                <div class="input-name">
+                    <input type="radio" name="radiogroup1" id="gender" class="radio-button">
+                    <label for="gender" class="gender">Male</label>
+                    
+                    <input type="radio" name="radiogroup1" id="genderFemale" class="radio-button">
+                    <label for="genderFemale" class="gender">Female </label>
+                </div>
+                
+                
+
+
+                <div class="input-name">
+                    <input class="button" type="submit" value="Registration">
+                </div>
+
+
+            </form>
+
+        </div>
+    </div>
+    </div>
+
+    </div>
+    
+
+
+</body></html>
